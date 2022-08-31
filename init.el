850cf0bbfb1aefaaff16f0789ef0172e866f48f9
Erik Wahlberger <ewahlberger@gmail.com>
2022-08-31 17:17:48 +0200

    Add basic packages and package configs

--- init.el
+++ init.el
@@ -9,7 +9,7 @@
 
 (set-face-attribute 'default nil :font "FiraCode NF" :height 120)
 
-(load-theme 'tango-dark)
+(load-theme 'doom-gruvbox)
 
 ;; Initialize package sources
 (require 'package)
@@ -29,4 +29,85 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
+;; Make ESC quit prompts
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+;; Packages
 (use-package command-log-mode)
+(use-package counsel
+	:bind (("M-x" . counsel-M-x)
+				 ("C-x b" . counsel-ibuffer)
+				 ("C-x C-f" . counsel-find-file)
+				 :map minibuffer-local-map
+				 ("C-r" . counsel-minibuffer-history)))
+(use-package ivy
+	:diminish
+	:bind (("C-s" . swiper)
+				 :map ivy-minibuffer-map
+				 ("TAB" . ivy-alt-done)
+				 ("C-l" . ivy-alt-done)
+				 ("C-j" . ivy-next-line)
+				 ("C-k" . ivy-previous-line)
+				 :map ivy-switch-buffer-map
+				 ("C-k" . ivy-previous-line)
+				 ("C-l" . ivy-done)
+				 ("C-d" . ivy-switch-buffer-kill)
+				 :map ivy-reverse-i-search-map
+				 ("C-k" . ivy-previous-line)
+				 ("C-d" . ivy-reverse-i-search-kill))
+	:config
+	(ivy-mode 1))
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1)
+	:custom ((doom-modeline-height 30)))
+(use-package doom-themes)
+(use-package all-the-icons
+  :if (display-graphic-p)
+	:ensure t)
+(use-package rainbow-delimiters
+	:hook (prog-mode . rainbow-delimiters-mode))
+(use-package which-key
+	:init (which-key-mode)
+	:diminish which-key-mode
+	:config
+	(setq which-key-idle-delay 0))
+(use-package ivy-rich
+	:init
+	(ivy-rich-mode 1))
+(use-package helpful
+  :commands (helpful-callable helpful-variable helpful-command helpful-key)
+  :custom
+  (counsel-describe-function-function #'helpful-callable)
+  (counsel-describe-variable-function #'helpful-variable)
+  :bind
+  ([remap describe-function] . counsel-describe-function)
+  ([remap describe-command] . helpful-command)
+  ([remap describe-variable] . counsel-describe-variable)
+  ([remap describe-key] . helpful-key))
+
+;; Line numbers
+(column-number-mode)
+(setq display-line-numbers-type 'relative)
+(global-display-line-numbers-mode t)
+
+;; Disable line numbers for some modes
+(dolist (mode '(org-mode-hook
+								term-mode-hook
+								shell-mode-hook
+								eshell-mode-hook))
+	(add-hook mode (lambda () (display-line-numbers-mode 0))))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("e3daa8f18440301f3e54f2093fe15f4fe951986a8628e98dcd781efbec7a46f2" default))
+ '(package-selected-packages '(doom-modeline use-package ivy command-log-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
