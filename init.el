b0b9902cb4e534bc7d7867e86fdef5350768ba13
Erik Wahlberger <ewahlberger@gmail.com>
2022-09-08 17:53:59 +0200

    add missing package repos

--- init.el
+++ init.el
@@ -10,13 +10,18 @@
 
 (electric-pair-mode)
 
+(require 'package)
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
 (package-initialize)
 (unless package-archive-contents
-	(package-refresh-contents))
+        (package-refresh-contents))
 
 ;; Initialize use-package on non-Linux platforms
 (unless (package-installed-p 'use-package)
-	(package-install 'use-package))
+        (package-install 'use-package))
 
 (require 'use-package)
 (setq use-package-always-ensure t)
