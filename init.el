c0e7b11dcff0c35b36fda86789a872e5dd1dd7b4
Erik Wahlberger <ewahlberger@gmail.com>
2022-09-08 17:56:02 +0200

    add no-littering package
    Keeps init.el clean

--- init.el
+++ init.el
@@ -26,6 +26,13 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
+(use-package no-littering)
+
+;; no-littering doesn't set this by default so we must place
+;; auto save files in the same path as it uses for sessions
+(setq auto-save-file-name-transforms
+      `((".*" ,(no-littering-expand-var-file-name "auto-save/") t)))
+
 (use-package vertico
   :init
   (vertico-mode))
