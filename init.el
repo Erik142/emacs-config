b0214a70a035fc3056a0a155b42f99fe82c51c68
Erik Wahlberger <EWahlberger@gmail.com>
2022-10-01 20:05:32 +0200

    Add straight.el package manager

--- init.el
+++ init.el
@@ -23,21 +23,25 @@
     (if (not (eq ON-WINDOWS nil))
         (if (eq IS-WORK t) WORK-NOTES-PATH PERSONAL-NOTES-PATH) "~/Denote"))
 
-(require 'package)
+;; (require 'package)
 
-(setq package-archives '(("melpa" . "https://melpa.org/packages/")
-                         ("org" . "https://orgmode.org/elpa/")
-                         ("elpa" . "https://elpa.gnu.org/packages/")))
-(package-initialize)
-(unless package-archive-contents
-        (package-refresh-contents))
-
-;; Initialize use-package on non-Linux platforms
-(unless (package-installed-p 'use-package)
-        (package-install 'use-package))
-
-(require 'use-package)
-(setq use-package-always-ensure t)
+;; (setq package-archives '(("melpa" . "https://melpa.org/packages/")
+;;                             ("org" . "https://orgmode.org/elpa/")
+;;                             ("elpa" . "https://elpa.gnu.org/packages/")))
+(defvar bootstrap-version)
+(let ((bootstrap-file
+        (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+        (bootstrap-version 6))
+    (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+            "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+            'silent 'inhibit-cookies)
+        (goto-char (point-max))
+        (eval-print-last-sexp)))
+    (load bootstrap-file nil 'nomessage))
+(straight-use-package 'use-package)
+(setq straight-use-package-by-default t)
 
 (use-package no-littering)
 
